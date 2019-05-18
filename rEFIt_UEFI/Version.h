233eb3bff27ed9d022e194193169c3618511b527
root <root@localhost.localdomain>
2019-05-18 06:59:35 -0400

    q35 speed patch

---
+++ rEFIt_UEFI/Version.h
@@ -0,0 +1,4 @@
+#define FIRMWARE_BUILDDATE "2019-05-17 23:54:04"
+#define FIRMWARE_REVISION L"4920"
+#define REVISION_STR "Clover revision: 4920"
+#define BUILDINFOS_STR "Args: --conf=/opt/Clover4920Mojave/edk2/Conf -D USE_LOW_EBDA -a X64 -b RELEASE -t GCC53 -n 17 | OS: CentOS 7.5.1804"
